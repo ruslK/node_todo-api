d8b670b327392528813fe9ca5210809a90c2c1d9
Ruslan <rkasymov@paypal.com>
2019-01-21 17:07:53 -0500

    Test POST /todos route

---
+++ server/tests/server.test.js
@@ -0,0 +1,58 @@
+const request = require('supertest');
+const expect = require('expect');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo')
+
+beforeEach((done) => {
+    Todo.deleteMany({}).then(() => {
+        done();
+    });
+})
+describe('POST /todos', () => {
+    it('should create a new Todo', (done) => {
+        let text = 'Test todo test'; 
+
+        request(app)
+        .post('/todos')
+        .send({text})
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.text).toBe(text);
+        })
+        .end((err, res) => {
+            if(err) {
+                done(err);
+            }
+
+            Todo.find().then((todos) => {
+                expect(todos.length).toBe(1);
+                expect(todos[0].text).toBe(text);
+                done();
+            }).catch((e) => {
+                done(e);
+            })
+        });
+    })
+
+    it('shoud not create todo with invalid body data', (done) => {
+        let text = ''; 
+
+        request(app)
+        .post('/todos')
+        .send({text})
+        .expect(400)
+        .end((err, res) => {
+            if(err) {
+                done(err);
+            }
+
+            Todo.find().then((todos) => {
+                expect(todos.length).toBe(0);
+                done();
+            }).catch((e) => {
+                done(e);
+            })
+        });
+    });
+});
